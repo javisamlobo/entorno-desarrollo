4c2e1bb947aa703b0e5f0a52c78932d5f9cd4871
javisamlobo <115726044+javisamlobo@users.noreply.github.com>
2022-10-13 18:55:42 +0200

    Final clase

--- src/com/javier/PcComponentes/TarjetaGrafica.java
+++ src/com/javier/PcComponentes/TarjetaGrafica.java
@@ -1,4 +1,11 @@
 package com.javier.PcComponentes;
 
 public class TarjetaGrafica {
+    /**Atributos*/
+    private Integer id;
+    private String marca;
+    private String modelo;
+    private String memoria;
+    /**Metodos*/
+
 }
