1c4936c79d098dc2e5934472a2d2db7bbfde61f4
javisamlobo <115726044+javisamlobo@users.noreply.github.com>
2022-10-17 18:18:40 +0200

    TarjetaGrafica

--- src/com/javier/PcComponentes/TarjetaGrafica.java
+++ src/com/javier/PcComponentes/TarjetaGrafica.java
@@ -7,5 +7,19 @@ public class TarjetaGrafica {
     private String modelo;
     private String memoria;
     /**Metodos*/
+    public Integer getId(){
+        return id;
+    }
+    public void  setId(Integer id) {
+        this.id = id;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public void setMarca(String marca) { this.marca = marca; }
+    public String getModelo(){ return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+    public String getMemoria(){ return memoria; }
+    public void setMemoria(String memoria) { this.memoria = memoria; }
 
 }
